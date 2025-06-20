aa011eeb791755bfb66b7010b74280005978e9a7
Muhammet Kaya <muham123cak@gmail.com>
2025-06-20 07:59:33 +0300

    SignUp.java

--- SignUp.java
+++ SignUp.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SignUp {
     public static void register() {
@@ -14,10 +16,29 @@ public class SignUp {
         JPasswordField passwordField = new JPasswordField();
         passwordField.setBounds(200, 160, 100, 40);
 
+        JButton registerButton = new JButton("Sign Up");
+        registerButton.setBounds(80, 65, 80, 30);
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getText());
+                String accountID = "ACC" + (int)(Math.random() * 100000);
+
+                Account newAccount = new Account(username, 1000, password, accountID);
+
+                DatabaseService dbService = new DatabaseService();
+                dbService.saveAccount(newAccount);
+
+                JOptionPane.showMessageDialog(signUpFrame, "Account created successfully!");
+            }
+        });
+
         signUpFrame.add(usernameLabel);
         signUpFrame.add(usernameField);
         signUpFrame.add(passwordLabel);
         signUpFrame.add(passwordField);
+        signUpFrame.add(registerButton);
 
         signUpFrame.setSize(400, 300);
         signUpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
